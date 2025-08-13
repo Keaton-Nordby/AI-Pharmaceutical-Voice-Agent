698c6844080d2c44e1435191bb5f8adc6716c81b
Keaton Nordby <keatonnordby4@gmail.com>
2025-08-12 20:25:41 -0500

    functionality of twilio handler and twilio receiver functions

--- main.py
+++ main.py
@@ -36,7 +36,37 @@ async def sts_receiver(sts_ws, twilio_ws, streamsid_queue):
     pass
 
 async def twilio_receiver(twilio_ws, audio_queue, streamsid_queue):
-    pass
+    BUFFER_SIZE = 20 * 160
+    inbuffer = bytearray(b"")
+
+    async for message in twilio_ws:
+        try:
+            data = json.loads(message)
+            event = data["event"]
+
+            if event == "start":
+                print("get our streamsid")
+                start = data["start"]
+                streamsid = start["streamsid"]
+                streamsid_queue.put_nowait(streamsid)
+            elif event == "connected":
+                continue
+            elif event == "media":
+                media = data["media"]
+                chunk = base64.b64decode(media["payload"])
+                if media["track"] == "inbound":
+                    inbuffer.extend(chunk)
+            elif event == "stop":
+                break
+
+            while len(inbuffer) >= BUFFER_SIZE:
+                chunk = inbuffer[:BUFFER_SIZE]
+                audio_queue.put_nowait(chunk)
+                inbuffer = inbuffer[BUFFER_SIZE:]
+
+        except:
+            break
+
 
 async def twilio_handler(twilio_ws):
     audio_queue = asyncio.Queue()
@@ -45,6 +75,16 @@ async def twilio_handler(twilio_ws):
     async  with sts_connect() as sts_ws:
         config_message = load_config()
         await sts_ws.send(json.dumps(config_message))
+
+        await asyncio.wait(
+            [
+                asyncio.ensure_future(sts_sender(sts_ws, audio_queue)),
+                asyncio.ensure_future(sts_receiver(sts_ws, twilio_ws, streamsid_queue)),
+                asyncio.ensure_future(twilio_receiver(twilio_ws, audio_queue, streamsid_queue)),
+            ]
+        )
+
+        await twilio_ws.close()
 
 async def main():
     await websockets.serve(twilio_handler, "localhost", 5000)
